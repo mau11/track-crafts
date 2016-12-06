eea0d7f1d3d4a5edea937392f96216fa392240f9
Maureen E. Mirville <mirvil7130@aol.com>
2016-12-05 22:17:01 -0800

    added css file, patterns component almost fully functional

--- app/patterns/patterns.js
+++ app/patterns/patterns.js
@@ -1,8 +1,20 @@
 angular.module('track-crafts.patterns',[])
   .controller('PatternsController', function($scope){
-    $scope.list= [{
-
-    }];
+    var allPatterns = [];
     $scope.listOfCrafts = ['KNIT', 'CROCHET', 'COMBO'];
-
+    $scope.item = {};
+    $scope.save = function() {
+      var patt = {};
+      if($scope.name !== undefined){//required field
+        patt.name= $scope.name;
+        patt.source=$scope.source;
+        patt.notes = $scope.notes;
+        patt.craft = $scope.craft;
+        allPatterns.push(patt);
+      }
+      console.log(allPatterns);
+    };
+    $scope.toggle = function(){
+      return allPatterns;
+    };
   });
